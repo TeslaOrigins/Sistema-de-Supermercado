0bd84c6d16aa28516fc8dd019e23f78270b5d9da
Paulo <>
2020-02-14 01:41:27 -0200

    Arquivo Estoque inicial

--- src/sistema_de_supermercado/Produto.java
+++ src/sistema_de_supermercado/Produto.java
@@ -1,5 +1,13 @@
 package sistema_de_supermercado;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Paulo
@@ -68,5 +76,46 @@ public class Produto {
     public void setSubtotal(double subtotal) {
         this.subtotal = subtotal;
     }
-
+    public String cadastrarArq(){
+        try{                                                   //Famoso TryCatch, caso dê erro na execução 
+           FileWriter fw= new FileWriter ("Estoque.txt",true); //Recebe o objeto que é o Estoque e escreve no arquivo
+           PrintWriter pw= new PrintWriter(fw);               //Quase a mesma coisa, vai entender
+           pw.println(this.codBarras+";"+this.nome+";"+this.preço+";"+this.qtd+";"+this.subtotal); //Escreve cada um em cada um
+           pw.flush();                                       //Grava o conteúdo do buffer no destino
+           pw.close();                                      //Fecha o fluxo permanente e libera recursos
+           fw.close();                                      
+            
+        }catch (IOException ex){    //serve para tratar as exceçoes(erros)  
+            Logger.getLogger(Produto.class.getName()).log(Level.SEVERE,null,ex);
+        }
+       return "Salvo com sucesso";
+    } /*
+    public String alterarArq(String compara) throws IOException {
+        try{                                                   
+            Scanner in = new Scanner(new File("Estoque.txt")); 
+            FileWriter fw = new FileWriter ("TEMP-ARQ",true);
+            StringBuilder arq = new StringBuilder();
+            String s;
+            while(in.hasNextLine()){
+                s = in.nextLine();
+                String[] sArray = s.split(";");
+                
+                if(!sArray[3].equals(compara)){
+                    arq.append(s);
+                    arq.append('\n');
+                }                
+            }
+            fw.write(arq.toString());
+            fw.write(this.codBarras+";"+this.nome+";"+this.preço+";"+this.qtd+";"+this.subtotal);
+            fw.flush();                                       
+            fw.close();                                      
+            
+            new File("Estoque.txt").delete();
+            new File("TEMP-ARQ").renameTo(new File("Estoque.txt"));            
+        }catch (IOException ex){     
+            Logger.getLogger(Pessoa.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        
+        return "Alterado com sucesso";
+    }*/
 }
